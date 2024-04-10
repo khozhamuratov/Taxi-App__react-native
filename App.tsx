02ccf79dfb0b41a8a7d5576ae71aff17e57aeec4
Nurman <khozhamuratov.nn@gmail.com>
2024-04-10 23:25:26 +0500

    feat: added push notifications

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {StatusBar, View} from 'react-native';
+import {Button, StatusBar, Text, View} from 'react-native';
 import {Provider} from 'react-redux';
 import IsAuthorization from './src/authorization';
 import Alert from './src/components/Alert/Alert';
 import {alertStyle} from './src/components/Alert/styles';
 import {store} from './src/redux/store';
+
+import LocalNotification from './src/localNotifications';
+import RemoteNotification from './src/RemoteNotifications';
 
 function App(): React.JSX.Element {
   return (
@@ -14,6 +17,9 @@ function App(): React.JSX.Element {
       <NavigationContainer>
         <IsAuthorization />
       </NavigationContainer>
+      <RemoteNotification />
+      <Text> Push Notification!! </Text>
+      <Button title={'Click Here'} onPress={LocalNotification} />
       <View style={alertStyle.container}>
         <Alert />
       </View>
